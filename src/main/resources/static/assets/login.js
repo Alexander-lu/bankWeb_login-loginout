7a79a4082bb560ec44f91be32ab1d5bfdb86931f
Alexander-lu <81604280@qq.com>
2022-06-22 09:15:46 +0000

    update src/main/resources/static/assets/login.js.

--- src/main/resources/static/assets/login.js
+++ src/main/resources/static/assets/login.js
@@ -33,7 +33,7 @@
    function isGood(data) {
     var patt1=new RegExp(/^1\d{10}$/);
     var patt2=new RegExp(/^[a-zA-Z._-]*[a-zA-Z]+[0-9]*@bank\Wc(om|n)$/);
-    var patt3=new RegExp(/-./);
+    var patt3=new RegExp(/[._-][._-]/);
     if(patt1.test(data)){
         return false;
     }else if(patt2.test(data)){
