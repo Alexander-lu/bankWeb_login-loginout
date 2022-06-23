53b73a1c613812eb4fa34a043f81c766cfe3b774
Alexander <81604280@qq.com>
2022-06-23 09:04:26 +0800

    w

--- spa/src/router.js
+++ spa/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/Login.vue'
+import balance from './views/balance.vue'
 
 Vue.use(Router)
 
@@ -10,7 +11,13 @@ export default new Router({
             path: '/login',
             name: 'Login',
             component: Login,   // `/login`路径访问Login组件
-        }, {
+        },
+        {
+            path: '/balance',
+            name: 'balance',
+            component: balance,   // `/login`路径访问balance组件
+        },
+        {
             path: '*',
             redirect: '/login'  // 所有路径都重定向到`/login`
         }
---
+++ spa/src/views/balance.vue
@@ -0,0 +1,72 @@
+<template>
+  <Card v-bind:image="require('../assets/account.png')">
+    <a>您当前余额为1234.56元</a>
+    <span v-if="errMsg">{{ errMsg }}</span>
+  </Card>
+</template>
+
+<script>
+import Card from "../components/Card.vue";
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: "",
+      password: "",
+      errMsg: ""
+    };
+  },
+  components: {
+    Card,
+  },
+  methods: {
+    login() {
+    },
+  },
+};
+</script>
+
+<style scoped>
+input {
+  display: block;
+  width: 70%;
+  height: 15%;
+  margin: 15px auto;
+  background: #fff;
+  border: 0px;
+  padding: 5px;
+  font-size: 16px;
+  border: 2px solid #fff;
+  transition: all 0.3s ease;
+  border-radius: 5px;
+}
+
+input:focus {
+  border: 2px solid #1abc9d;
+}
+
+input[type="submit"] {
+  display: block;
+  background: #1abc9d;
+  width: 70%;
+  padding: 4px;
+  cursor: pointer;
+  color: #fff;
+  border: 0px;
+  margin: auto;
+  border-radius: 5px;
+  font-size: 12px;
+  transition: all 0.3s ease;
+  line-height: 12px;
+}
+
+input[type="submit"]:hover {
+  background: #09cca6;
+}
+
+span {
+  margin: 0.5em;
+  color: red;
+}
+</style>
