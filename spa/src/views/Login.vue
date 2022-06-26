ad775320a948f5a49e43a9f675ca090b11785bdb
Alexander_Lu <81604280@qq.com>
2022-06-26 10:07:27 +0800

    修改了样式

--- spa/src/views/Login.vue
+++ spa/src/views/Login.vue
@@ -1,73 +1,105 @@
 <template>
-  <Card v-bind:image="require('../assets/bank.png')">
-    <input type="text" placeholder="用户名" v-model="username" />
-    <input type="password" placeholder="密码" v-model="password" />
+  <Card>
+    <el-carousel :interval="2000" type="card" height="562.5px" autoplay="true">
+      <el-carousel-item v-for="item in images" :key="item">
+        <img :src="item.url" alt="无图片" class="imageURL" />
+      </el-carousel-item>
+    </el-carousel>
+    <el-input class='what' placeholder="用户名" v-model="username" clearable></el-input>
+    <el-input class='what' placeholder="密码" v-model="password" show-password></el-input>
     <input type="submit" value="登陆" @click="login" />
     <span v-if="errMsg">{{ errMsg }}</span>
   </Card>
 </template>
 
 <script>
-import Card from "../components/Card.vue";
-
 export default {
   name: "Login",
   data() {
     return {
       username: "",
       password: "",
-      errMsg: ""
+      errMsg: "",
+      images: [
+        { url: require("../assets/1.png") },
+        { url: require("../assets/2.jpg") },
+        { url: require("../assets/3.png") },
+        { url: require("../assets/4.png") },
+        { url: require("../assets/5.jpg") },
+        { url: require("../assets/6.jpg") }
+      ],
     };
   },
   components: {
-    Card,
-  },
+},
   methods: {
     login() {
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', "/api/login", true);
-      xhr.setRequestHeader("Content-type", "application/json");
-      xhr.onreadystatechange = (e) =>  {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-          console.log(e);
-          if(JSON.parse(xhr.responseText).status=="good"){
-                this.$router.push('/balance');
-          }else {
-          this.errMsg = JSON.parse(xhr.responseText).errMsg
-          }
+      if(isNotGood(this.username)){
+      this.axios({
+        method:"POST",
+        url:"/api/login",
+        data:{
+            "username":this.username,
+            "password":encrypt(this.password)
         }
-      };
-      let a = this.password
-      xhr.send(JSON.stringify({ "username": this.username, "password": encrypt(a) }));
+       }).then(response=>{
+           if (response.data.status == "good") {
+            this.$router.push('/balance');
+          } else {
+            this.errMsg = response.data.errMsg
+          }
+       })
+      }else {
+     this.errMsg = "您输入的用户名有误！"
+      }
     },
   },
 };
+/**
+ * 对密码进行加密
+ * @param {string} passwd 用户输入的密码
+ * @returns {string} 加密后的密码
+ */
+function encrypt(passwd) {
+  var PASSWORD_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz";
+  var newStr = "";
+  for (let i = 0; i < passwd.length; i++) {
+    var posInCipherText = PASSWORD_ALPHABET.indexOf(passwd.charAt(i));
+    var posInPlainText = (36 + (posInCipherText - 9)) % 36;
+    newStr += PASSWORD_ALPHABET.charAt(posInPlainText)
+  }
+  // console.log(newStr);
+  return newStr;
+}
   /**
-   * 对密码进行加密
-   * @param {string} passwd 用户输入的密码
-   * @returns {string} 加密后的密码
+   * 判断输入是不是非法的
    */
-  function encrypt(passwd) {
-    var PASSWORD_ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyz";
-    var newStr = "";
-    for (let i = 0; i < passwd.length; i++) {
-      var posInCipherText = PASSWORD_ALPHABET.indexOf(passwd.charAt(i));
-      var posInPlainText = (36+(posInCipherText - 9)) % 36;
-      newStr += PASSWORD_ALPHABET.charAt(posInPlainText)
-    }
-    // console.log(newStr);
-    return newStr;
+   function isNotGood(data) {
+    var patt1=new RegExp(/^1\d{10}$/);
+    var patt2=new RegExp(/^[a-zA-Z._-]*[a-zA-Z]+[0-9]*@bank.c(om|n)$/);
+    var patt3=new RegExp(/[._-][._-]/);
+    if(patt1.test(data)){
+        return true;
+    }else if(patt2.test(data)){
+        if(data.length>30){
+          return false;
+        }
+        if(patt3.test(data)){
+          return false;
+        }
+        return true;
+      }else{
+        return false;
+      }
   }
 </script>
 
 <style scoped>
-input {
-  display: block;
+.what {
   width: 70%;
   height: 15%;
   margin: 15px auto;
   background: #fff;
-  border: 0px;
   padding: 5px;
   font-size: 16px;
   border: 2px solid #fff;
@@ -75,11 +107,12 @@ input {
   border-radius: 5px;
 }
 
-input:focus {
+what:focus {
   border: 2px solid #1abc9d;
 }
 
 input[type="submit"] {
+  height: 50px;
   display: block;
   background: #1abc9d;
   width: 70%;
@@ -88,6 +121,7 @@ input[type="submit"] {
   color: #fff;
   border: 0px;
   margin: auto;
+  margin-bottom: 20px;
   border-radius: 5px;
   font-size: 12px;
   transition: all 0.3s ease;
@@ -101,5 +135,38 @@ input[type="submit"]:hover {
 span {
   margin: 0.5em;
   color: red;
+}
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n+1) {
+  background-color: #d3dce6;
+}
+
+.imageURL {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  min-width: 1000px;
+  z-index: -10;
+  zoom: 1;
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
 }
 </style>
--- spa/src/views/balance.vue
+++ spa/src/views/balance.vue
@@ -1,67 +1,57 @@
 <template>
-  <Card v-bind:image="require('../assets/account.png')">
-  <p >欢迎回来，{{ message }}！</p>
-  <a>您当前余额为1234.56元</a>
-  <input type="submit" value="退出登陆" @click="loginout" />
-  </Card>
+<Card>
+    <el-menu :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect"
+      background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+      <el-menu-item index="1"><a href="https://www.bankofchina.com/pbservice/pb3/">金融理财</a></el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">个人中心</template>
+        <el-menu-item index="2-1"><a class="bai" href="https://www.bankofchina.com/bcservice/">银行卡</a></el-menu-item>
+        <el-menu-item index="2-2"><a class="bai" href="https://www.bankofchina.com/pbservice/pb6/">私人银行</a></el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+    </el-menu>
+    <p>欢迎回来，{{ message }}！</p>
+    <a>您当前余额为1234.56元</a>
+    <input type="submit" value="退出登陆" @click="loginout" />
+</Card>
 </template>
 
 <script>
-import Card from "../components/Card.vue";
 import cookies from 'vue-cookies'
 export default {
   name: "balance",
   data() {
     return {
       message: "",
+      activeIndex: '1',
+      activeIndex2: '1'
     };
   },
-  created(){
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', "/api/name", true);
-                xhr.setRequestHeader("Content-type", "application/json");
-                xhr.onreadystatechange = (e) =>  {
-                  if (xhr.readyState == 4 && xhr.status == 200) {
-                    console.log(e);
-                    if(JSON.parse(xhr.responseText).status=="good"){
-                        this.message=JSON.parse(xhr.responseText).name
-                    }
-                  }
-                };
-                xhr.send();
-  }
-  ,
-  components: {
-    Card,
+  created() {
+    //发送get
+    this.axios({
+      method: "GET",
+      url: "/api/name"
+    }).then(response => {
+      console.log(response);
+      this.message = response.data.name
+    })
   },
+  components: {
+},
   methods: {
-  loginout(){
-    cookies.remove("session_id");
-    this.$router.push('/Login');
-  }
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    loginout() {
+      cookies.remove("session_id");
+      this.$router.push('/Login');
+    }
   },
 };
 </script>
 
 <style scoped>
-input {
-  display: block;
-  width: 70%;
-  height: 15%;
-  margin: 15px auto;
-  background: #fff;
-  border: 0px;
-  padding: 5px;
-  font-size: 16px;
-  border: 2px solid #fff;
-  transition: all 0.3s ease;
-  border-radius: 5px;
-}
-
-input:focus {
-  border: 2px solid #1abc9d;
-}
-
 input[type="submit"] {
   display: block;
   background: #1abc9d;
@@ -84,5 +74,11 @@ input[type="submit"]:hover {
 span {
   margin: 0.5em;
   color: red;
+}
+a{
+   text-decoration:none 
+}
+.bai{
+  color:#fff
 }
 </style>
